2c664cedd36cd6c61ad461dbdb95e0428ff813cb
Julen Urizar Compains <julenuri@hotmail.com>
2026-03-30 12:01:28 +0200

    [KERNEL32][KERNEL32_APITEST] Fix the pipe buffer overflow situation into Iosb.Status in the ReadFile (#6724)
    Change the way ReadFile and NTReadFile behaves under ReactOS, and fit more what Wine does. Fixes Calibre conversion failure. JIRA issue: CORE-17376

--- dll/win32/kernel32/client/file/rw.c
+++ dll/win32/kernel32/client/file/rw.c
@@ -101,7 +101,6 @@ WriteFile(IN HANDLE hFile,
          * completed).
          */
         *lpNumberOfBytesWritten = Iosb.Information;
-
         if (!NT_SUCCESS(Status))
         {
             BaseSetLastNTError(Status);
