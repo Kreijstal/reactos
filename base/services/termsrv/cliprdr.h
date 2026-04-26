2b46e0746020959478e6f4c5871a7e74e0dd276f
Kreijstal <rainb@tfwno.gf>
2026-04-27 01:10:01 +0200

    [TERMSRV] Improve RDP cliprdr integration

--- base/services/termsrv/cliprdr.h
+++ base/services/termsrv/cliprdr.h
@@ -16,13 +16,32 @@
 #define TERMSRV_CLIPRDR_CB_FORMAT_DATA_REQUEST   0x0004
 #define TERMSRV_CLIPRDR_CB_FORMAT_DATA_RESPONSE  0x0005
 #define TERMSRV_CLIPRDR_CB_CLIP_CAPS             0x0007
+#define TERMSRV_CLIPRDR_CB_FILECONTENTS_REQUEST  0x0008
+#define TERMSRV_CLIPRDR_CB_FILECONTENTS_RESPONSE 0x0009
+#define TERMSRV_CLIPRDR_CB_LOCK_CLIPDATA         0x000a
+#define TERMSRV_CLIPRDR_CB_UNLOCK_CLIPDATA       0x000b
 
 #define TERMSRV_CLIPRDR_CB_RESPONSE_OK           0x0001
 #define TERMSRV_CLIPRDR_CB_RESPONSE_FAIL         0x0002
+#define TERMSRV_CLIPRDR_CB_ASCII_NAMES           0x0004
+
+#define TERMSRV_CLIPRDR_CB_CAPSTYPE_GENERAL      0x0001
+#define TERMSRV_CLIPRDR_CB_CAPS_VERSION_2        0x00000002
+#define TERMSRV_CLIPRDR_CB_USE_LONG_FORMAT_NAMES 0x00000002
+#define TERMSRV_CLIPRDR_CB_STREAM_FILECLIP_ENABLED 0x00000004
+#define TERMSRV_CLIPRDR_CB_FILECLIP_NO_FILE_PATHS 0x00000008
+#define TERMSRV_CLIPRDR_CB_CAN_LOCK_CLIPDATA     0x00000010
+#define TERMSRV_CLIPRDR_CB_HUGE_FILE_SUPPORT_ENABLED 0x00000020
 
 #define TERMSRV_CLIPRDR_CHANNEL_NAME             "cliprdr"
 #define TERMSRV_CLIPRDR_MAX_CHANNEL_NAME_LENGTH  8
 #define TERMSRV_CLIPRDR_INVALID_CHANNEL_ID       0
+
+#define TERMSRV_CLIPRDR_CF_UNICODETEXT           13
+#define TERMSRV_CLIPRDR_CF_DIB                   8
+#define TERMSRV_CLIPRDR_CF_DIBV5                 17
+#define TERMSRV_CLIPRDR_MAX_FORMATS              32
+#define TERMSRV_CLIPRDR_MAX_FORMAT_NAME          64
 
 typedef enum _TERMSRV_CLIPRDR_RESULT
 {
@@ -67,6 +86,8 @@ typedef struct _TERMSRV_CLIPRDR_BACKEND
 {
     const TERMSRV_CLIPRDR_BACKEND_OPS *Ops;
     VOID *Context;
+    ULONG PendingFormatId;
+    BOOL ReplaceOnNextSet;
 } TERMSRV_CLIPRDR_BACKEND;
 
 #define TERMSRV_CLIPRDR_DUMMY_MAX_DATA_LENGTH    4096
@@ -78,6 +99,25 @@ typedef struct _TERMSRV_CLIPRDR_DUMMY_BACKEND
     SIZE_T DataLength;
     UCHAR Data[TERMSRV_CLIPRDR_DUMMY_MAX_DATA_LENGTH];
 } TERMSRV_CLIPRDR_DUMMY_BACKEND;
+
+typedef struct _TERMSRV_CLIPRDR_WIN32_BACKEND
+{
+    HANDLE WindowStation;
+    HANDLE Desktop;
+    BOOL Attached;
+} TERMSRV_CLIPRDR_WIN32_BACKEND;
+
+typedef struct _TERMSRV_CLIPRDR_FORMAT
+{
+    ULONG FormatId;
+    WCHAR Name[TERMSRV_CLIPRDR_MAX_FORMAT_NAME];
+} TERMSRV_CLIPRDR_FORMAT;
+
+typedef struct _TERMSRV_CLIPRDR_FORMAT_LIST
+{
+    ULONG Count;
+    TERMSRV_CLIPRDR_FORMAT Formats[TERMSRV_CLIPRDR_MAX_FORMATS];
+} TERMSRV_CLIPRDR_FORMAT_LIST;
 
 typedef struct _TERMSRV_CLIPRDR_PDU
 {
@@ -156,6 +196,15 @@ TermSrvCliprdrDummyBackendReset(
     _Out_ TERMSRV_CLIPRDR_DUMMY_BACKEND *Dummy);
 
 TERMSRV_CLIPRDR_RESULT
+TermSrvCliprdrWin32BackendInit(
+    _Out_ TERMSRV_CLIPRDR_WIN32_BACKEND *Win32,
+    _Out_ TERMSRV_CLIPRDR_BACKEND *Backend);
+
+TERMSRV_CLIPRDR_RESULT
+TermSrvCliprdrWin32BackendAttach(
+    _Inout_ TERMSRV_CLIPRDR_WIN32_BACKEND *Win32);
+
+TERMSRV_CLIPRDR_RESULT
 TermSrvCliprdrParsePdu(
     _In_reads_bytes_(BufferLength) const UCHAR *Buffer,
     _In_ SIZE_T BufferLength,
@@ -168,10 +217,47 @@ TermSrvCliprdrWriteMonitorReady(
     _Out_ SIZE_T *BytesWritten);
 
 TERMSRV_CLIPRDR_RESULT
+TermSrvCliprdrWriteCapabilities(
+    _Out_writes_bytes_to_(BufferLength, *BytesWritten) UCHAR *Buffer,
+    _In_ SIZE_T BufferLength,
+    _Out_ SIZE_T *BytesWritten);
+
+TERMSRV_CLIPRDR_RESULT
 TermSrvCliprdrWriteFormatListResponse(
     _Out_writes_bytes_to_(BufferLength, *BytesWritten) UCHAR *Buffer,
     _In_ SIZE_T BufferLength,
     _In_ USHORT ResponseFlags,
+    _Out_ SIZE_T *BytesWritten);
+
+TERMSRV_CLIPRDR_RESULT
+TermSrvCliprdrParseFormatList(
+    _In_reads_bytes_(BufferLength) const UCHAR *Buffer,
+    _In_ SIZE_T BufferLength,
+    _Out_ TERMSRV_CLIPRDR_FORMAT_LIST *FormatList);
+
+BOOL
+TermSrvCliprdrFormatListContains(
+    _In_ const TERMSRV_CLIPRDR_FORMAT_LIST *FormatList,
+    _In_ ULONG FormatId);
+
+ULONG
+TermSrvCliprdrFindNamedFormat(
+    _In_ const TERMSRV_CLIPRDR_FORMAT_LIST *FormatList,
+    _In_z_ const WCHAR *Name);
+
+TERMSRV_CLIPRDR_RESULT
+TermSrvCliprdrWriteFormatList(
+    _Out_writes_bytes_to_(BufferLength, *BytesWritten) UCHAR *Buffer,
+    _In_ SIZE_T BufferLength,
+    _In_reads_(FormatCount) const TERMSRV_CLIPRDR_FORMAT *Formats,
+    _In_ ULONG FormatCount,
+    _Out_ SIZE_T *BytesWritten);
+
+TERMSRV_CLIPRDR_RESULT
+TermSrvCliprdrWriteFormatDataRequest(
+    _Out_writes_bytes_to_(BufferLength, *BytesWritten) UCHAR *Buffer,
+    _In_ SIZE_T BufferLength,
+    _In_ ULONG FormatId,
     _Out_ SIZE_T *BytesWritten);
 
 TERMSRV_CLIPRDR_RESULT
