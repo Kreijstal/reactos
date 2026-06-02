00cf1854db507fce77b7927145fb02bb78cb9655
Kreijstal <rainb@tfwno.gf>
2026-06-02 19:18:25 +0200

    [FREELDR] Emit ACPI_BIOS_MULTI_NODE for the ACPI BIOS configuration data
    DetectAcpiBios now fills the shared ACPI_BIOS_MULTI_NODE layout from
    <reactos/drivers/acpi/acpi.h> instead of the local ACPI_BIOS_DATA shape:
    record the RSDP address, store the RSDT/XSDT pointer in RsdtAddress, and
    copy the BIOS memory map into ACPI_E820_ENTRY records (Base/Length/Type)
    rather than a raw BIOS_MEMORY_MAP blob. The table size is recomputed for
    the new node so the registry resource descriptor matches what the ACPI
    driver expects.

--- boot/freeldr/freeldr/arch/i386/hwacpi.c
+++ boot/freeldr/freeldr/arch/i386/hwacpi.c
@@ -19,6 +19,7 @@
  */
 
 #include <freeldr.h>
+#include <reactos/drivers/acpi/acpi.h>
 
 #include <debug.h>
 DBG_DEFAULT_CHANNEL(HWDETECT);
@@ -62,8 +63,9 @@ DetectAcpiBios(PCONFIGURATION_COMPONENT_DATA SystemKey, ULONG *BusNumber)
     PCM_PARTIAL_RESOURCE_LIST PartialResourceList;
     PCM_PARTIAL_RESOURCE_DESCRIPTOR PartialDescriptor;
     PRSDP_DESCRIPTOR Rsdp;
-    PACPI_BIOS_DATA AcpiBiosData;
+    PACPI_BIOS_MULTI_NODE AcpiBiosData;
     ULONG TableSize, Size;
+    ULONG i;
 
     Rsdp = FindAcpiBios();
 
@@ -73,8 +75,8 @@ DetectAcpiBios(PCONFIGURATION_COMPONENT_DATA SystemKey, ULONG *BusNumber)
         AcpiPresent = TRUE;
 
         /* Calculate the table size */
-        TableSize = PcBiosMapCount * sizeof(BIOS_MEMORY_MAP) +
-            sizeof(ACPI_BIOS_DATA) - sizeof(BIOS_MEMORY_MAP);
+        TableSize = sizeof(ACPI_BIOS_MULTI_NODE) +
+                    (PcBiosMapCount - 1) * sizeof(ACPI_E820_ENTRY);
 
         /* Set 'Configuration Data' value */
         Size = FIELD_OFFSET(CM_PARTIAL_RESOURCE_LIST, PartialDescriptors[1]) + TableSize;
@@ -96,22 +98,27 @@ DetectAcpiBios(PCONFIGURATION_COMPONENT_DATA SystemKey, ULONG *BusNumber)
         PartialDescriptor->u.DeviceSpecificData.DataSize = TableSize;
 
         /* Fill the table */
-        AcpiBiosData = (PACPI_BIOS_DATA)(PartialDescriptor + 1);
+        AcpiBiosData = (PACPI_BIOS_MULTI_NODE)(PartialDescriptor + 1);
+        AcpiBiosData->RsdpAddress.QuadPart = (ULONG_PTR)Rsdp;
 
         if (Rsdp->revision > 0)
         {
             TRACE("ACPI >1.0, using XSDT address\n");
-            AcpiBiosData->RSDTAddress.QuadPart = Rsdp->xsdt_physical_address;
+            AcpiBiosData->RsdtAddress.QuadPart = Rsdp->xsdt_physical_address;
         }
         else
         {
             TRACE("ACPI 1.0, using RSDT address\n");
-            AcpiBiosData->RSDTAddress.LowPart = Rsdp->rsdt_physical_address;
+            AcpiBiosData->RsdtAddress.LowPart = Rsdp->rsdt_physical_address;
         }
 
         AcpiBiosData->Count = PcBiosMapCount;
-        RtlCopyMemory(AcpiBiosData->MemoryMap, PcBiosMemoryMap,
-                      PcBiosMapCount * sizeof(BIOS_MEMORY_MAP));
+        for (i = 0; i < PcBiosMapCount; i++)
+        {
+            AcpiBiosData->E820Entry[i].Base.QuadPart = PcBiosMemoryMap[i].BaseAddress;
+            AcpiBiosData->E820Entry[i].Length.QuadPart = PcBiosMemoryMap[i].Length;
+            AcpiBiosData->E820Entry[i].Type = PcBiosMemoryMap[i].Type;
+        }
 
         TRACE("RSDT %p, data size %x\n", Rsdp->rsdt_physical_address,
             TableSize);
