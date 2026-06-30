1f60e8dae9545972278e6213d1a3ae4027531b08
Kreijstal <rainb@tfwno.gf>
2026-06-30 16:40:10 +0200

    [NTGDI] Include advanced-mode arc bounds

--- win32ss/gdi/ntgdi/arc.c
+++ win32ss/gdi/ntgdi/arc.c
@@ -114,6 +114,14 @@ IntArc( DC *dc,
     RectSEpts.right   += dc->ptlDCOrig.x;
     RectSEpts.bottom  += dc->ptlDCOrig.y;
 
+    if ((pdcattr->iGraphicsMode == GM_ADVANCED) &&
+        (pdcattr->mxWorldToDevice.flAccel & XFORM_UNITY))
+    {
+        RECTL_vMakeWellOrdered(&RectBounds);
+        RectBounds.right++;
+        RectBounds.bottom++;
+    }
+
     DPRINT("1: StartX: %d, StartY: %d, EndX: %d, EndY: %d\n",
                RectSEpts.left,RectSEpts.top,RectSEpts.right,RectSEpts.bottom);
 
