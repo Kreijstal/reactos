55a065f2d4ee08c6ee4f461b9b4ce3b3be713de2
Kreijstal <rainb@tfwno.gf>
2026-08-31 17:23:00 +0200

    [WINED3D] Widen the vertex-attribute offset before casting to a pointer
    FIELD_OFFSET yields a LONG and MSVC treats the direct cast to void* as
    C4312 (an error) on 64-bit; go through ULONG_PTR.

--- dll/directx/wine/wined3d/context_gl.c
+++ dll/directx/wine/wined3d/context_gl.c
@@ -5178,7 +5178,7 @@ void wined3d_context_gl_draw_shaded_quad(struct wined3d_context_gl *context_gl, 
         GL_EXTCALL(glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STREAM_DRAW));
         GL_EXTCALL(glVertexAttribPointer(0, 2, GL_FLOAT, FALSE, sizeof(*quad), NULL));
         GL_EXTCALL(glVertexAttribPointer(1, 3, GL_FLOAT, FALSE, sizeof(*quad),
-                (void *)FIELD_OFFSET(struct blit_vertex, texcoord)));
+                (void *)(ULONG_PTR)FIELD_OFFSET(struct blit_vertex, texcoord)));
 
         GL_EXTCALL(glEnableVertexAttribArray(0));
         GL_EXTCALL(glEnableVertexAttribArray(1));
